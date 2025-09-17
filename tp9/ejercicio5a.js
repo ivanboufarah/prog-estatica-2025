0e93d66cb31bdb7a774bf44c5c67067a12852ac1
Ivan Boufarah <ivanboufarah@gmail.com>
2025-09-17 20:24:05 -0300

    subir tp9

---
+++ tp9/ejercicio5a.js
@@ -0,0 +1,12 @@
+function suma (n1,n2){
+    return n1 + n2;
+}
+function resta (n1,n2){
+    return n1 - n2;
+}
+function multiplicacion (n1,n2){
+    return n1 * n2;
+}
+function division (n1,n2){
+    return n1 / n2;
+} 
